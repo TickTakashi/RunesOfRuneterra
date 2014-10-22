632e470940144f37b3ac3d056a27732a579d8028
Charles TickTakashi Gbadamosi <TakashiGbadamosi@hotmail.com>
2014-10-22 04:10:55 +0100

    EffectCompiler class wasn't added last time, added now.

---
+++ RUNES/EffectCompiler.cs
@@ -0,0 +1,25 @@
+﻿using Antlr4.Runtime;
+using CARDScript.Compiler;
+using CARDScript.Compiler.Effects;
+using CARDScript.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CARDScript {
+  class EffectCompiler {
+    public static Effect Compile(IGameController controller, Player user,
+      ICard source) {
+      AntlrInputStream input_stream = new AntlrInputStream(
+        source.GetCARDScript());
+      CARDScriptLexer lexer = new CARDScriptLexer(input_stream);
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+      CARDScriptParser parser = new CARDScriptParser(tokens);
+      CARDScriptParser.EffectContext effect = parser.effect();
+      EffectVisitor effect_visitor = new EffectVisitor(user, 
+        controller.Opponent(user), source);
+      return effect.Accept<Effect>(effect_visitor);
+    }
+  }
+}
