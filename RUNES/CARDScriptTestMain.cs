afd58b1d620824a81379234d27288169226782c7
Charles TickTakashi Gbadamosi <TakashiGbadamosi@hotmail.com>
2014-10-22 04:22:00 +0100

    Adds dummy classes for testing and completes Compile in EffectCompiler

--- RUNES/CARDScriptTestMain.cs
+++ RUNES/CARDScriptTestMain.cs
@@ -1,6 +1,7 @@
 ﻿using Antlr4.Runtime;
 using CARDScript.Compiler;
 using CARDScript.Compiler.Effects;
+using CARDScript.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,36 +11,56 @@ using System.Text;
 namespace CARDScript {
   class CARDScriptTestMain {
     public static void Main() {
-      string input =  "WHEN ENEMY HEALS >= 1 {\n" +
-                      "  ENEMY TAKES 1 3 TIMES \n" +
-                      "  USER HEALS 3\n" +
-                      "} 3 CHARGES";
+
 
       Console.WriteLine("Compiling this:");
-      Console.WriteLine(input);
-      Console.WriteLine();
-      Effect compiled = Compile(input);
+      DummyCard d = new DummyCard("WHEN ENEMY HEALS >= 1 {\n" +
+                                  "  ENEMY TAKES 1 3 TIMES \n" +
+                                  "  USER HEALS 3\n" +
+                                  "} 3 CHARGES");
+      DummyPlayer a = new DummyPlayer();
+      DummyPlayer b = new DummyPlayer();
+      DummyGameController g = new DummyGameController(a, b);
+      Effect compiled = EffectCompiler.Compile(g, a, d);
+
       Console.WriteLine("Compiled object: " + compiled);
 
       Console.WriteLine();
       Console.WriteLine("Testing ScheduleEffect");
       compiled.Activate();
     }
+  }
 
-    public static Effect Compile(string input) {
-      AntlrInputStream input_stream = new AntlrInputStream(input);
-      CARDScriptLexer lexer = new CARDScriptLexer(input_stream);
-      CommonTokenStream tokens = new CommonTokenStream(lexer);
-      CARDScriptParser parser = new CARDScriptParser(tokens);
-      EffectVisitor visitor = new EffectVisitor(new DummyPlayer(), 
-                                                new DummyPlayer(),
-                                                new DummyCard());
-      CARDScriptParser.EffectContext effect = parser.effect();
-      return effect.Accept<Effect>(visitor);
+  class DummyCard : ICard {
+    string description;
+    
+    public DummyCard(string s) {
+      description = s;
+    }
+
+    public string GetCARDScript() {
+      return description;
     }
   }
 
-  class DummyCard : ICard { }
+  class DummyGameController : IGameController {
+    Player user;
+    Player enemy;
+
+    public DummyGameController(Player user, Player enemy) {
+      this.user = user;
+      this.enemy = enemy;
+    }
+
+    public void Schedule(Compiler.Events.GameEventListener listener) {
+      throw new NotImplementedException();
+    }
+
+    public Player Opponent(Player p) {
+      return p == user ? enemy : user;
+    }
+  }
+
   class DummyPlayer : Player {
 
     public override void Damage(int value) {
